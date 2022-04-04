34ff884e051e3f1ec692b54ee023dffb67b86280
Eneko Trigo <02enekotrigo@gmail.com>
2022-04-04 09:09:14 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'RecuperacionFinal_EnekoTrigo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
